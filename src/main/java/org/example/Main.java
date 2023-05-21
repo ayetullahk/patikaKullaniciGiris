45031f68b7ad1ed712e9dba6a2933610cbc67da5
ayetullahkarabulut <karabulutayetullah@gmail.com>
2023-05-21 15:16:33 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String userNama,password;
+        String userNama,password,newPassword,response;
 
         Scanner inp = new Scanner(System.in);
 
@@ -14,7 +14,18 @@ public class Main {
         if (userNama.equals("Ayetullah")&& password.equals("Karabulut")){
             System.out.println("Hoşgeldiniz :)");
         }else {
-            System.out.println("Giriş Başarısız");
+            System.out.println("Giriş Başarısız Şifre sınırlamak için Evet veya Hayır:");
+            response=inp.nextLine();
+            if (response.equals("Evet")){
+                System.out.print("Yeni Şifrenizi giriniz:");
+                newPassword=inp.nextLine();
+                if (newPassword.equals(password) ) {
+                    System.out.println("Giriş başarılı Şifre oluşturuldu");
+                }else {
+                    System.out.println("Yeni şifre eski şifreden farklı olmalıdır");
+                }
+
+            }
         }
 
     }
